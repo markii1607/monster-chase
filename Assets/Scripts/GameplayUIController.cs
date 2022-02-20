40c3942176548451d94c2eb9f1faaec14bffa407
Mark Jerly Bundalian <mjbundalian61@gmail.com>
2022-02-21 01:21:44 +0800

    Add script for Restart and Home buttons

---
+++ Assets/Scripts/GameplayUIController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameplayUIController : MonoBehaviour
+{
+    public void RestartButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void HomeButton()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
