e0f369d5a2b5acff66fba2062fd0b2ec07c99be4
Mark Jerly Bundalian <mjbundalian61@gmail.com>
2022-02-20 23:46:55 +0800

    Create instance of GameManager and do not destroy onload

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,18 +4,34 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+
     [SerializeField]
     private GameObject[] characters;
 
-    // Start is called before the first frame update
-    void Start()
+    private int _charIndex;
+    public int CharIndex
     {
-        
+        get
+        {
+            return _charIndex;
+        }
+        set
+        {
+            _charIndex = value;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }
